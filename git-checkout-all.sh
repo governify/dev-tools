75884a33ac7ddb9304ae6b066aeb019728257007
CesarGP <cgpcsjcmp@gmail.com>
2021-06-30 13:37:49 +0200

    fix: checkout

--- git-checkout-all.sh
+++ git-checkout-all.sh
@@ -1,5 +1,5 @@
 for d in */ ; do
     echo "======================================================================="   
     echo "Checking out repo $d. Branch name: $1"
-    (cd $d; git checkout origin/$1)
+    (cd $d; git checkout --track origin/$1)
 done
